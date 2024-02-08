2ee1f2596ba27e99437cf08e8ea62e2b669c38b1
Admin <1q.expert@gmail.com>
2024-02-08 19:36:21 +0200

    fix header hidden/change color, fix achievemnt, fix arrow partners

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,52 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.achievement_list li').forEach(function(listItem) {
+  let isAnimating = false;
+
+  document.querySelectorAll('.achievement_list li').forEach(function(listItem) {
       listItem.addEventListener('mouseover', function() {
-        // Hide all images first by removing the 'visible' class
-        document.querySelectorAll('.achievement_images img').forEach(function(img) {
-          img.classList.remove('visible');
-        });
-        
-        // Show the corresponding image by adding the 'visible' class
-        var imageId = 'image' + this.id.replace('achievement', '');
-        var imageElement = document.getElementById(imageId);
-        if (imageElement) {
-          imageElement.classList.add('visible');
-        }
+          if (isAnimating) return; 
+          isAnimating = true;
+
+          document.querySelectorAll('.achievement_images img').forEach(function(img) {
+              img.classList.remove('visible');
+          });
+
+          var imageId = 'image' + this.id.replace('achievement', '');
+          var imageElement = document.getElementById(imageId);
+          if (imageElement) {
+              imageElement.classList.add('visible');
+              setTimeout(() => {
+                  isAnimating = false;
+              }, 800); 
+          }
       });
-    
-      listItem.addEventListener('mouseout', function() {
-        // Hide the image again by removing the 'visible' class
-        var imageId = 'image' + this.id.replace('achievement', '');
-        var imageElement = document.getElementById(imageId);
-        if (imageElement) {
-          imageElement.classList.remove('visible');
-        }
-      });
-    });
   });
-  
-//хедер
+});
+
+
 let lastScrollY = window.scrollY;
 const header = document.getElementById('header');
-const menuBtnMobile = document.querySelector('.menu-btn-mobile'); // Находим кнопку по классу
+const menuBtnMobile = document.querySelector('.menu-btn-mobile');
 
-// Добавляем обработчик события на прокрутку
 window.addEventListener('scroll', () => {
-  if (lastScrollY < window.scrollY) {
+  const mainPageTop = document.querySelector('.main_page')?.offsetTop || 0; // 
+  console.log(mainPageTop);
+
+  if (window.scrollY > lastScrollY) {
     header.style.top = '-100px';
-    header.classList.add('inverse');
+    if (window.scrollY > 200) {
+      header.classList.add('inverse');
+    }
   } else {
     header.style.top = '0';
+    if (window.scrollY <= mainPageTop || window.scrollY <= 200) {
+      header.classList.remove('inverse');
+    }
   }
 
   lastScrollY = window.scrollY;
 });
 
-// Добавляем обработчик события на клик по кнопке
 menuBtnMobile.addEventListener('click', () => {
-  header.style.top = '-100%'; // Полное скрытие хедера
-  // Опционально: можно убрать класс 'inverse', если он мешает визуальному стилю при скрытом хедере
+  header.style.top = '-100%';
   header.classList.remove('inverse');
 });

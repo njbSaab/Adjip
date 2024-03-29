07cfcaf8d58959d994e77c2ca434a5ea16178b57
Admin <1q.expert@gmail.com>
2024-03-29 23:37:51 +0200

    last comm

--- src/js/moduls-js/header.js
+++ src/js/moduls-js/header.js
@@ -3,9 +3,12 @@ class HeaderModule {
     this.lastScrollY = window.scrollY;
     this.header = document.getElementById("header");
     this.overlaySidenav = document.getElementById("offcanvas-flip");
-
     this.menuBtnMobile = document.querySelector(".menu-btn-mobile");
     this.closeBtn = document.querySelector(".uk-offcanvas-close");
+    this.mainPageTop = document.querySelector(".main_page")?.offsetTop || 0;
+    this.offcanvas = UIkit.offcanvas(this.overlaySidenav);
+    this.sideNav = document.querySelector(".side-nav");
+    this.offcanvasBar = document.querySelector(".uk-offcanvas-bar");
     this.init();
   }
 
@@ -15,11 +18,7 @@ class HeaderModule {
     this.addCloseButtonListener();
     this.addOverlayListener();
     this.addSideNavListener();
-    this.console();
-  }
-
-  console() {
-    console.log();
+    this.addMenuButtonHoverListener();
   }
 
   throttle(func, limit) {
@@ -37,43 +36,50 @@ class HeaderModule {
 
   addScrollListener() {
     const throttledScroll = this.throttle(() => {
-      const mainPageTop = document.querySelector(".main_page")?.offsetTop || 0;
-
       if (window.scrollY > this.lastScrollY) {
-        this.header.style.top = "-100px";
+        this.header.style.top = "-100px"; // Сначала изменяем позицию
         if (window.scrollY > 0) {
-          this.header.classList.add("inverse");
+          setTimeout(() => { // Добавляем задержку перед добавлением класса 'inverse'
+            this.header.classList.add("inverse");
+          }, 150); // Задержка должна быть больше, чем время анимации скрытия заголовка
         }
       } else {
-        this.header.style.top = "0";
-        if (window.scrollY <= mainPageTop || window.scrollY <= 100) {
-          this.header.classList.remove("inverse");
+        if (window.scrollY <= this.mainPageTop || window.scrollY <= 100) {
+          this.header.classList.remove("inverse"); // Удаляем класс 'inverse' сразу
         }
+        this.header.style.top = "0"; // Затем изменяем позицию
       }
-
+  
       this.lastScrollY = window.scrollY;
     }, 100);
-
+  
     window.addEventListener("scroll", throttledScroll);
+  }
+  
+
+  setHeaderStyle(top, inverseAction) {
+    this.header.style.top = top;
+    this.header.classList[inverseAction]("inverse");
+  }
+
+  addMenuButtonHoverListener() {
+    this.menuBtnMobile.addEventListener('mouseenter', () => {
+      this.offcanvas.show();
+      this.setHeaderStyle("-100%", "remove");
+    });
   }
 
   addMenuButtonListener() {
     this.menuBtnMobile.addEventListener("click", () => {
-      this.header.style.top = "-100%";
-      this.header.classList.remove("inverse");
+      this.setHeaderStyle("-100%", "remove");
     });
   }
 
   addSideNavListener() {
-    const sideNav = document.querySelector(".side-nav");
-    const offcanvasBar = document.querySelector(".uk-offcanvas-bar");
-
-    if (sideNav && offcanvasBar) {
-      sideNav.addEventListener("click", (event) => {
-        if (!offcanvasBar.contains(event.target)) {
-          this.header.classList.add("inverse");
-          this.header.style.top = "0";
-          this.header.style.transition = "top .7s ease";
+    if (this.sideNav && this.offcanvasBar) {
+      this.sideNav.addEventListener("click", (event) => {
+        if (!this.offcanvasBar.contains(event.target)) {
+          this.setHeaderStyle("0", "add");
         }
       });
     }
@@ -82,25 +88,20 @@ class HeaderModule {
   addCloseButtonListener() {
     if (this.closeBtn) {
       this.closeBtn.addEventListener("click", () => {
-        this.header.classList.add("inverse");
-        this.header.style.top = "0";
-        this.header.style.transition = "top .7s ease";
+        this.setHeaderStyle("0", "add");
       });
     }
   }
 
   addOverlayListener() {
     this.overlaySidenav.addEventListener("click", () => {
-      const sidenaveInParent = document.querySelector(".uk-offcanvas-bar");
-      if (!sidenaveInParent) {
-        this.header.classList.add("inverse");
-        this.header.style.top = "0";
-        this.header.style.transition = "top .7s ease";
+      if (!document.querySelector(".uk-offcanvas-bar")) {
+        this.setHeaderStyle("0", "add");
       }
     });
   }
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   new HeaderModule();
 });
